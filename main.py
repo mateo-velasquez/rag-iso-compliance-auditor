1bf9f8e16c2ce304b3b32a2ac4688ea6b3e6c477
mateo-velasquez <mateovelasquezborda04@gmail.com>
2025-12-19 10:48:15 -0300

    agregué leve frontend

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import uvicorn
 from app import create_app
+from fastapi.middleware.cors import CORSMiddleware
 
 app = create_app()
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # En producción se pone el dominio real, para pruebas "*" está bien
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Run server
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+
